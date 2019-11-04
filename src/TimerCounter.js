42dec84da2caa708131247f454461b1f70a5b157
Joel Noir <noir.joel@gmail.com>
2019-11-04 14:59:15 +0100

    Refactor State TimeCounter
    Delete derive state to calculate state

--- src/TimerCounter.js
+++ src/TimerCounter.js
@@ -1,44 +1,24 @@
 import React, { Fragment } from "react";
 
 class TimerCounter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      minutesCounter: 0,
-      secondsCounter: 0
-    };
+  humanMinutesTimer(counterTimer) {
+    return Math.floor(counterTimer / 60);
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.counterTimeTimer !== prevProps.counterTimeTimer) {
-      // TODO:: Améliorer
-      if (prevProps.counterTimeTimer > this.props.counterTimeTimer + 2) {
-        let minutesCounter = Math.floor(this.props.counterTimeTimer / 60);
-
-        this.setState(() => ({
-          minutesCounter: minutesCounter,
-          secondsCounter: 0
-        }));
-        return;
-      }
-
-      if (
-        this.state.secondsCounter !== 0 &&
-        this.state.secondsCounter % 59 === 0
-      ) {
-        this.setState(state => ({
-          minutesCounter: state.minutesCounter + 1,
-          secondsCounter: 0
-        }));
-      } else {
-        this.setState(state => ({ secondsCounter: state.secondsCounter + 1 }));
-      }
+  humanSecondsTimer(counterTimer) {
+    if (counterTimer < 59) {
+      return counterTimer;
+    } else {
+      let minutesHuman = Math.floor(counterTimer / 60);
+      let secondsHuman = counterTimer - minutesHuman * 60;
+      return secondsHuman;
     }
   }
 
   render() {
-    let minCounter = this.state.minutesCounter;
-    let secCounter = this.state.secondsCounter;
+    //Todo:: Améliorer
+    let minCounter = this.humanMinutesTimer(this.props.counterTimeTimer);
+    let secCounter = this.humanSecondsTimer(this.props.counterTimeTimer);
     if (minCounter <= 9) {
       minCounter = "0" + minCounter;
     }
@@ -47,7 +27,11 @@ class TimerCounter extends React.Component {
     }
 
     let show_timer;
-    show_timer = minCounter + ":" + secCounter;
+    if (this.props.counterTimeTimer > 0) {
+      show_timer = minCounter + ":" + secCounter;
+    } else {
+      show_timer = "00:00";
+    }
 
     return (
       <Fragment>
